bddf02b7d5a3c2bd609383a43c8328397b72f0b2
MAURICIO PEREIRA FACIROLLI BRANQUINHO <mauricio.branquinho@senacsp.edu.br>
2025-09-27 10:44:49 -0300

    subindo aula

---
+++ IntroducaoJS/ola-mundo-cra/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Olá ReactJS!</h1>
+        <h2>Este é o curso de ReactJS</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
